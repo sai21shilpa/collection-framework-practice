67247ad3bb1fcaa93d8cc76c67ef2abbec4bbddc
sai21shilpa <saishilpa.komma@gmail.com>
2023-12-20 17:28:10 -0600

    Update MaxWealth.java

--- MaxWealth.java
+++ MaxWealth.java
@@ -1,1 +1,31 @@
- 
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int[][] arr={
+            {1,2,3},
+            {4,1,6},
+            {3,3,7}
+        };
+        int a=maximumWealth(arr);
+        System.out.print(a);
+    }
+    static int maximumWealth(int[][] arr)
+    {
+       
+        int res=Integer.MIN_VALUE;
+        for(int[] ints:arr)
+        {
+              int sum=0;
+            for(int accountAmount:ints)
+            {
+                sum=sum+accountAmount;
+            }
+            if(sum>res)
+            {
+                res=sum;
+            }
+            
+        }
+        return res;
+    }
+}
